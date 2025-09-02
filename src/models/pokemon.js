026f06cc56f66225785ad7aff1a2c0bfd27f46fc
leukapr <leukapr@gmail.com>
2025-09-02 17:43:16 +0200

    Ajout Constrainte unicité de la propriété name

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -17,6 +17,7 @@ module.exports = (sequelize, DataTypes) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: { msg: "Le nom est déjà pris." },
       validate: {
         notEmpty: { msg: "Le nom ne peut pas être vide." },
         notNull: { msg: "Le nom est une propriété requise." }
