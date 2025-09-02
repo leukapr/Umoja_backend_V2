2e181be89dff0c44d4cce41b8a4ef6185d036ea3
leukapr <leukapr@gmail.com>
2025-09-02 17:19:02 +0200

    Initialisation des validateurs isEmpty,notNull,isUrl

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -8,19 +8,35 @@ module.exports = (sequelize, DataTypes) => {
     },
     name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isEmpty: { msg: "Le nom ne peut pas être vide." },
+        notNull: { msg: "Le nom est une propriété requise." }
+      }
     },
     hp: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isInt: { msg: "Utilisez uniquement des nombres entiers pour les points de vie." },
+        notNull: { msg: "Les points de vie sont une propriété requise." }
+      }
     },
     cp: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isInt: { msg: "Utilisez uniquement des nombres entiers pour les points de dégât." },
+        notNull: { msg: "Les points de dégât sont une propriété requise." }
+      } 
     },
     picture: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isUrl: { msg: "Utilisez uniquement une URL valide pour l'image." },
+        notNull: { msg: "L'URL de l'image est une propriété requise." }
+      }
     },
     types: {
       type: DataTypes.STRING,
--- src/routes/updatePokemon.js
+++ src/routes/updatePokemon.js
@@ -17,6 +17,9 @@ module.exports = (app) => {
         });
       })
       .catch((error) => {
+        if(error instanceof ValidationError) {
+          return res.status(400).json({ message: error.message, data: error });
+        }
         const message =
           "Le pokémon n'a pas pu être modifié. Réessayez dans quelques instants.";
         res.status(500).json({ message, data: error });
