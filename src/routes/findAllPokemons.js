4c6e1dc920ef3a7e95b834225c1e7d42b6291c51
leukapr <leukapr@gmail.com>
2025-09-02 18:27:24 +0200

    Initialisation des opérateur sequelize pour pour effectuer une recherche partielle

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,15 +1,26 @@
 const { Pokemon } = require("../db/sequelize");
+const { Op } = require("sequelize");
 
 module.exports = (app) => {
   app.get("/api/pokemons", (req, res) => {
+    // Ici on va chercher si un paramètre de requête "name" est présent
     if (req.query.name) {
+      
       const name = req.query.name;
-      return Pokemon.findAll({ where: { name } })
+      // On utilise la méthode findAll de Sequelize pour récupérer les pokémons
+      return Pokemon.findAll({
+        where: {
+          name: {
+            [Op.eq]: name}
+        }
+      })
+        // On gère la promesse retournée par findAll
         .then((pokemons) => {
           const message = `Il y a ${pokemons.length} pokémons correspondant au terme de recherche ${name}.`;
           res.json({ message, data: pokemons });
         })
     }
+    // Si pas de paramètre "name", on récupère tous les pokémons
     Pokemon.findAll().then((pokemons) => {
       const message = "La liste des pokémons a bien été récupérée.";
       res.json({ message, data: pokemons });
