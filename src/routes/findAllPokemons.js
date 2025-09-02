d3bbddaf9b9be277cc9c4b055db3882b0dfd68e8
leukapr <leukapr@gmail.com>
2025-09-02 18:12:04 +0200

    Extraction du parametre de requete name dans findAllPokemons.js

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -2,6 +2,14 @@ const { Pokemon } = require("../db/sequelize");
 
 module.exports = (app) => {
   app.get("/api/pokemons", (req, res) => {
+    if (req.query.name) {
+      const name = req.query.name;
+      return Pokemon.findAll({ where: { name } })
+        .then((pokemons) => {
+          const message = `La liste des pokémons correspondant au nom ${name} a bien été récupérée.`;
+          res.json({ message, data: pokemons });
+        })
+    }
     Pokemon.findAll().then((pokemons) => {
       const message = "La liste des pokémons a bien été récupérée.";
       res.json({ message, data: pokemons });
