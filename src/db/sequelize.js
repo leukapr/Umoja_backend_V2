7b18c01b2a165c1e7c2da29ab2415ae7b45c791b
leukapr <leukapr@gmail.com>
2025-09-02 19:45:19 +0200

    Import model user, on linstancie comme le model Pokemon, ensuite on pousse un utlisateur dans la DB grâce à la methode create de sequelize

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,6 +1,7 @@
 /* L’API Rest et la Base de données : Créer un modèle Sequelize */
 const { Sequelize, DataTypes } = require('sequelize')
 const PokemonModel = require('../models/pokemon')
+const UserModel = require('../models/user') 
 const pokemons = require('./mock-pokemon')
   
 const sequelize = new Sequelize('pokedex', 'root', '', {
@@ -13,6 +14,8 @@ const sequelize = new Sequelize('pokedex', 'root', '', {
 })
   
 const Pokemon = PokemonModel(sequelize, DataTypes)
+const User = UserModel(sequelize, DataTypes)
+
   
 const initDb = () => {
   return sequelize.sync({force: true}).then(_ => {
@@ -25,6 +28,11 @@ const initDb = () => {
         types: pokemon.types
       }).then(pokemon => console.log(pokemon.toJSON()))
     })
+
+    User.create({
+      username: 'admin',
+      password: 'admin'
+    }).then(user => console.log(user.toJSON()))
     console.log('La base de donnée a bien été initialisée !')
   })
 }
